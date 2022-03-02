90176af1dcead1d3613b63bb5186e49bf1f4a223
hassaannnn <hassaan3@ualberta.ca>
2022-03-02 15:11:20 -0700

    update

--- mp2.py
+++ mp2.py
@@ -23,24 +23,41 @@ def connect(path):
     connection.commit()
     return connection, cursor
 
-def attemptSignIn(user,pwd):
-    print(user+pwd)
+
+def attemptSignIn(user, pwd):
+    print(user + pwd)
+    return 0
+
+
+def signUpNewUser():
+    path = "miniproj2.db"
+    global connection, cursor
+    connection = sqlite3.connect(path)
+    cursor = connection.cursor()
+    newUserName = input("Enter your name")
+    newUserID = input("Enter your desired ID")
+    newUserPassword = input("Enter desired password")
+    cursor.execute("INSERT INTO CUSTOMERS VALUES (?, ?, ?)",(newUserID,newUserName,newUserPassword))
+    connection.commit()
+
+
 
 def signinscreen():
     print("1.Login\n2.Register")
     userSignInChoice = input()
+    # Handle singin in
     if userSignInChoice == "1":
-        #While loop to keep looping until correct user and password entered
-        while(True):
-            signInUserName  = input("Enter Id: ")
+        # While loop to keep looping until correct user and password entered
+        while True:
+            signInUserName = input("Enter Id: ")
             signInPassword = input("Enter password: ")
-            signInResult = attemptSignIn(signInUserName,signInPassword)
-            if(signInResult == 0):
+            signInResult = attemptSignIn(signInUserName, signInPassword)
+            if signInResult == 0:
                 break
             else:
                 print("Incorrect information, please try again! ")
-
-
+    elif userSignInChoice == "2":
+        signUpNewUser()
 
 
 if __name__ == '__main__':
