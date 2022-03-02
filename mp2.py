4d8c35caaee97fad0ad3b7fdd15fbbeaeccf3f34
hassaannnn <hassaan3@ualberta.ca>
2022-03-02 14:55:34 -0700

    update

--- mp2.py
+++ mp2.py
@@ -1,9 +1,13 @@
+import genericpath
 import sqlite3
+import os.path
+from os import path
 import time
 import hashlib
+import logging
 
 
-def setupDatabase(path="miniproj2.db"):
+def setupdatabase(path="miniproj2.db"):
     print(path)
     print("created database at path:", path)
     connect(path)
@@ -19,5 +23,29 @@ def connect(path):
     connection.commit()
     return connection, cursor
 
+def attemptSignIn(user,pwd):
+    print(user+pwd)
+
+def signinscreen():
+    print("1.Login\n2.Register")
+    userSignInChoice = input()
+    if userSignInChoice == "1":
+        #While loop to keep looping until correct user and password entered
+        while(True):
+            signInUserName  = input("Enter Id: ")
+            signInPassword = input("Enter password: ")
+            signInResult = attemptSignIn(signInUserName,signInPassword)
+            if(signInResult == 0):
+                break
+            else:
+                print("Incorrect information, please try again! ")
+
+
+
+
 if __name__ == '__main__':
-    setupDatabase()
+    if (path.exists("miniproj2.db")):
+        logging.info("Database already exists")
+    else:
+        setupdatabase()
+    signinscreen()
